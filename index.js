c21a36ce0938846abde245891dae411d5c9c1d00
Nardos Getachew <nardos1g3@gmail.com>
2022-01-30 13:56:27 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+cards = document.querySelector('.cards')
+console.log(cards)
+let card = cards;
+h3.delete();
+
+document.querySelector('.cards') = card;
+let users = [1, 2, 3, 4];
+
+for(let i=0; i<=users.length; i++){
+
+};
---
+++ practice.js
@@ -0,0 +1,6 @@
+let str = " ";
+for( let i = 1; i<=64; i++){
+    // either str += "  ";
+    // let's say it starts with space;
+}
+console.log(str)
